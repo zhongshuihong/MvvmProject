b464fba8456ca7b79807bd47e1e7f27aed39ba3a
zhongshuihong <zhongsh_2011@sina.cn>
2022-04-18 01:14:06 +0800

    delete

--- app/src/main/java/com/alphabet/mvvmbinding/BindingFragment.kt
+++ app/src/main/java/com/alphabet/mvvmbinding/BindingFragment.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.alphabet.mvvmbinding.databinding.FragmentBindingBinding
 
-// TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -19,7 +18,6 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class BindingFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -34,7 +32,7 @@ class BindingFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         val binding = DataBindingUtil.inflate<FragmentBindingBinding>(
             inflater,
@@ -56,7 +54,6 @@ class BindingFragment : Fragment() {
          * @param param2 Parameter 2.
          * @return A new instance of fragment BindingFragment.
          */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             BindingFragment().apply {
